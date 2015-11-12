7d8983b3692a5a8db3153e614efe8ab76d95ba10
Eurico Costa <ejcosta.byui@gmail.com>
2015-11-12 12:51:37 -0700

    Baseline version
    Baseline version of the site.

---
+++ modules/footer.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<div> 
+    <div class="barcopy">
+       <div class="bar-wrapcopy">
+           <div class="copyright">This content is in the public domain. Please give credit to <a href="mailto:ejcosta@gmail.com">me</a> if you use it.</div>
+            <div class="copyright">Last Updated: 18 October, 2015</div>
+       </div>
+    </div>
+</div>
+
+
+
+
---
+++ modules/header.php
@@ -0,0 +1,20 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<div>
+    <div class="logo">
+        <a href="/CIT-261-PersonalPortfolio/" title="CIT-261 Fall Semester 2015 - Understanding Porfolio">
+            <h1>Eagle sightings Tracker</h1>
+            <h2>
+                Team 4 - Group Project<br>
+                CIT-261 Fall Semester 2015</h2>
+            <hr>
+        </a>
+    </div>
+</div>
